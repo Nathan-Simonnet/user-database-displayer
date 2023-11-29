af7e77de387c925220193335d7c4f77cebce5add
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-11-29 11:07:10 +0100

    TODO: template

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // https://randomuser.me/api/?results=24
-console.log("userdb")
+console.log("userdb");
+
+
+
+
+// Quentin: results.name.first
+// Maurel: results.name.last
+
+// Glasgow: results.location.city
+// 12/12/1994: results.dob.date
+
+// registered: results.registered.date
+
+
