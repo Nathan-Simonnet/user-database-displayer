b37b79e84a45c3f06f01020fcdc185bd3aac4883
Nathan-Simonnet <nathan.simonnet@gmail.com>
2023-11-30 10:31:48 +0100

    template and fetch done, TODO UX

--- script.js
+++ script.js
@@ -2,14 +2,69 @@
 console.log("userdb");
 
 
+let data = [];
+
+const userDisplayer = function (data) {
 
 
-// Quentin: results.name.first
-// Maurel: results.name.last
+    for (let i = 0; i < data.length; i++) {
 
-// Glasgow: results.location.city
-// 12/12/1994: results.dob.date
+        const currentDateOfBirth = data[i].dob.date;
 
-// registered: results.registered.date
+        const dobParser = function (dateOfBirth) {
+            let dobArray = dateOfBirth.split("T");
+            let dobArrayParsed = dobArray[0].split("-")
+            let finalDateOfBirth = [];
+            // console.log(dobArrayParsed)
+            for (let i = 0; i < dobArrayParsed.length; i++) {
+                finalDateOfBirth.unshift(dobArrayParsed[i])
+                console.log(dobArrayParsed.length, i, dobArrayParsed[i])
+            }
+
+            return finalDateOfBirth.join("/")
+        }
 
 
+        // Quentin: name.first
+        // Maurel: name.last
+
+        // Glasgow: location.city
+        // 12/12/1994: dob.date
+
+        // registered: registered.date
+
+        document.querySelector('main').innerHTML += `
+    
+    <div class="card">
+    <div class="bc-container">
+        <div class="bc-up"></div>
+        <div class="bc-down"> </div>
+    </div>
+    <div class="info-container">
+    <div class="img-container">
+            <img src="${data[i].picture.medium}" alt="${data[i].name.first}" picture>
+        </div>
+        <h3>${data[i].name.first} + ${data[i].name.last}</h3>
+        <p>${data[i].location.city}</p>
+        <p>${dobParser(currentDateOfBirth)}</p>
+        <p>fnsf</p>
+    </div>
+    </div>
+    
+    `}
+
+}
+
+const userFetcher = async function () {
+    fetch('https://randomuser.me/api/?results=24 ')
+        .then((response) => response.json())
+        .then((responseJson) => {
+
+            data = responseJson.results
+            console.log(data)
+            userDisplayer(data);
+        })
+}
+
+
+userFetcher();
